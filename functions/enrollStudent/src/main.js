c42f61039daec909761e00f2be2312da98d94b7a
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2024-01-04 19:13:48 +0300

    feat(functions): implement function to enroll user for a program

--- functions/enrollStudent/src/main.js
+++ functions/enrollStudent/src/main.js
@@ -1,33 +1,107 @@
-import { Client } from 'node-appwrite';
+import {
+  Client,
+  Teams,
+  Databases,
+  Permission,
+  Role,
+  ID,
+  Query,
+} from 'node-appwrite';
 
-// This is your Appwrite function
-// It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //   .setEndpoint('https://cloud.appwrite.io/v1')
-  //   .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //   .setKey(process.env.APPWRITE_API_KEY);
+  const client = new Client()
+    .setEndpoint(process.env.APPWRITE_FUNCTION_PROJECT_ENDPOINT)
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_FUNCTION_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+  const teams = new Teams(client);
+  const database = new Databases(client);
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+  const { programId, organisation, userId } = JSON.parse(req.body);
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
+  log(`Program Id: ${programId}`);
+  log(`Organisation: ${organisation}`);
+  log(`User Id: ${userId}`);
+  log(`Body: ${req.body}`);
+
+  const permissions = [
+    Permission.read(Role.user(userId)),
+    Permission.update(Role.user(userId)),
+    Permission.read(Role.team(organisation, 'admin')),
+    Permission.read(Role.team(organisation, 'owner')),
+    Permission.update(Role.team(organisation, 'admin')),
+    Permission.update(Role.team(organisation, 'owner')),
+  ];
+
+  try {
+    const membership = await teams.createMembership(
+      organisation,
+      ['student'],
+      process.env.APPWRITE_FUNCTION_PROJECT_ENDPOINT,
+      undefined,
+      userId
+    );
+
+    log(JSON.stringify(membership, null, 2));
+
+    const enrolment = await database.createDocument(
+      process.env.APPWRITE_FUNCTION_DATABASE_ID,
+      process.env.APPWRITE_FUNCTION_ENROLMENTS_COLLECTION_ID,
+      ID.unique(),
+      {
+        userId,
+        programId,
+      },
+      permissions
+    );
+
+    log(JSON.stringify(enrolment, null, 2));
+
+    return res.json({
+      ok: true,
+      message: `Congratulations. You've successfuly enrolled for the program`,
+      enrolment,
+    });
+  } catch (err) {
+    error(err);
+    const { total: currentProgramEnrolments } = await database.listDocuments(
+      process.env.APPWRITE_FUNCTION_DATABASE_ID,
+      process.env.APPWRITE_FUNCTION_ENROLMENTS_COLLECTION_ID,
+      [
+        Query.equal('completionStatus', [
+          'pendingPayment',
+          'inProgress',
+          'pendingApproval',
+          'completed',
+        ]),
+        Query.equal('programId', [programId]),
+        Query.equal('userId', [userId]),
+      ]
+    );
+
+    log(`Number of current program Enrolments: ${currentProgramEnrolments}`);
+    if (currentProgramEnrolments === 0) {
+      const enrolment = await database.createDocument(
+        process.env.APPWRITE_FUNCTION_DATABASE_ID,
+        process.env.APPWRITE_FUNCTION_ENROLMENTS_COLLECTION_ID,
+        ID.unique(),
+        {
+          userId,
+          programId,
+        },
+        permissions
+      );
+
+      return res.json({
+        ok: true,
+        message: `Congratulations. You've successfuly enrolled for the program`,
+        enrolment,
+      });
+    }
+
+    return res.json({
+      ok: true,
+      message: `You are already enrolled in this program`,
+    });
   }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
 };
